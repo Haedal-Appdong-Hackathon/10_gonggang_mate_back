2c2f5e449c6542a1cdfa488faa3a02c34446bf0b
Kjh-0629 <89411104+Kjh-0629@users.noreply.github.com>
2023-11-24 23:52:37 +0900

    [feat]: 게ì 게시판 목록 보ê¸

--- src/main/java/gonggang/server/post/PostController.java
+++ src/main/java/gonggang/server/post/PostController.java
@@ -1,8 +1,13 @@
 package gonggang.server.post;
 
+import java.util.List;
+
+import gonggang.server.common.entity.Post;
 import lombok.RequiredArgsConstructor;
 import  lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +23,9 @@ public class PostController {
         @RequestBody PostForm form
     ){
         postService.post(form);
+    }
+    @GetMapping("/board")
+    public List<Post> getBoard(){
+        return postService.getBoard();
     }
 }
--- src/main/java/gonggang/server/post/PostRepository.java
+++ src/main/java/gonggang/server/post/PostRepository.java
@@ -2,6 +2,6 @@ package gonggang.server.post;
 
 import gonggang.server.common.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long>{
-
 }
